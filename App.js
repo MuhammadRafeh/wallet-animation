6bb449d83b8e429112004e2d670c4feec8241472
MuhammadRafeh <muhammadrafehatique@gmail.com>
2020-12-30 20:50:01 +0300

    Visible Done

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import Card from './Card.js';
+
+const cards = [
+  {
+    name: "Shot",
+    color: "#a9d0b6",
+    price: "30 CHF"
+  },
+  {
+    name: "Juice",
+    color: "#e9bbd1",
+    price: "64 CHF"
+  },
+  {
+    name: "Mighty Juice",
+    color: "#eba65c",
+    price: "80 CHF"
+  },
+  {
+    name: "Sandwich",
+    color: "#95c3e4",
+    price: "85 CHF"
+  },
+  {
+    name: "Combi",
+    color: "#1c1c1c",
+    price: "145 CHF"
+  },
+  {
+    name: "Signature",
+    color: "#a390bc",
+    price: "92 CHF"
+  },
+  {
+    name: "Coffee",
+    color: "#fef2a0",
+    price: "47 CHF"
+  }
+];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={StyleSheet.absoluteFill}>
+        {
+          cards.map(card => <Card key={card.name} {...card}/>)
+        }
+      </View>
+      <ScrollView />
     </View>
   );
 }
